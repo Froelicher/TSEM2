a78c075b8685a41747035478a31aad0fe16555ec
Froelicher <froelicher.jeanphilippe@gmail.com>
2014-12-10 12:39:00 +0100

    web tv + twitch dans doc test api twitch

---
+++ Sources/Test API/TestTwitchWrapper/TestTwitchWrapper/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Twitch.Net;
+
+namespace TestTwitchWrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Twitch.Net.Model.Stream yolo = new Twitch.Net.Model.Stream();
+            
+        }
+    }
+}
